caae3584af4413bea2b0f219931c0454fe72b997
Acong <handsd6111@gmail.com>
2022-06-02 07:11:35 +0000

    所有的表

---
+++ database/migrations/2022_06_02_040638_create_members_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // 成員 table
+        Schema::create('members', function (Blueprint $table) {
+            $table->char('id', 13)->primary()->comment('成員編號');
+            $table->string('account', 30)->comment('成員帳號');
+            $table->string('name', 30)->comment('成員姓名');
+            $table->string('password', 35)->comment('成員密碼');
+            $table->string('avatar', 50)->nullable()->comment('成員頭像');
+            $table->string('introduction')->nullable()->comment('成員自介');
+            $table->string('phone', 30)->commnet('成員手機號碼');
+            $table->integer('authority')->comment('成員權限');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
---
+++ database/migrations/2022_06_02_045708_create_wallets_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWalletsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id()->comment('錢包編號');
+            $table->string('name', 20)->comment('錢包名稱');
+            $table->decimal('balance')->comment('錢包餘額');
+            $table->string('description')->nullable()->comment('錢包備註');
+            $table->timestamps();
+            $table->char('member_id', 13)->comment('成員編號');
+        });
+        Schema::table('wallets', function (Blueprint $table) {
+            $table->foreign('member_id')->references('id')->on('members');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets', function (Blueprint $table) {
+            $table->dropForeign('member_id');
+        });
+    }
+}
